dd4007d702e9080ebcf281c04c3aa05bcb4b8b4c
Ajaymakwana123 <ammakwana4150@gmail.com>
2025-09-07 22:15:18 +0530

    Changes

--- src/components/home/HomeHeroText.jsx
+++ src/components/home/HomeHeroText.jsx
@@ -3,18 +3,18 @@ import Video from './Video'
 
 const HomeHeroText = () => {
     return (
-        <div className='font-[font1]  mt-72 lg:mt-0 pt-5 text-center'>
-            <div className='lg:text-[9.5vw] text-[12vw] justify-center flex items-center uppercase lg:leading-[8vw] leading-[10vw]'>
+        <div className='font-[font1]  mt-56 lg:mt-4 pt-5 text-center'>
+            <div className='lg:text-[8.1vw] text-[12vw] justify-center flex items-center uppercase lg:leading-[8vw] leading-[10vw]'>
                 L'étincelle
             </div>
-            <div className='lg:text-[9.5vw] text-[12vw] justify-center flex items-start uppercase lg:leading-[8vw] leading-[10vw]'>
+            <div className='lg:text-[8.1vw] text-[12vw] justify-center flex items-start uppercase lg:leading-[8vw] leading-[10vw]'>
                 qui
                 <div className='h-[7vw] w-[16vw] rounded-full lg:-mt-3 mt-0 overflow-hidden '>
                     <Video />
                 </div>
                 génère
             </div>
-            <div className='lg:text-[9.5vw] text-[12vw] justify-center flex items-center uppercase lg:leading-[8vw] leading-[10vw]'>
+            <div className='lg:text-[8.1vw] text-[12vw] justify-center flex items-center uppercase lg:leading-[8vw] leading-[10vw]'>
                 la créativité
             </div>
         </div>
